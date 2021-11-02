ccab881ba484953e448abf268f24397efe3c8035
alekhinefan <szavinm@yahoo.com>
2021-11-02 08:52:24 +0100

    rebuild in es5

--- index.js
+++ index.js
@@ -4,46 +4,49 @@ var CellState;
     CellState[CellState["Dead"] = 0] = "Dead";
     CellState[CellState["Live"] = 1] = "Live";
 })(CellState || (CellState = {}));
-class Cell {
-    constructor(coordinates) {
+var Cell = /** @class */ (function () {
+    function Cell(coordinates) {
         this.currentState = this.decideInitialState();
         this.nextState = CellState.Dead;
         this.coordinates = coordinates;
     }
-    decideInitialState() {
-        const rnd = Math.floor(Math.random() * 10);
+    Cell.prototype.decideInitialState = function () {
+        var rnd = Math.floor(Math.random() * 8);
         return rnd === 5 ? CellState.Live : CellState.Dead;
-    }
-    getCurrentState() {
+    };
+    Cell.prototype.getCurrentState = function () {
         return this.currentState;
-    }
-    setNextState(cellState) {
+    };
+    Cell.prototype.setNextState = function (cellState) {
         this.nextState = cellState;
-    }
-}
-class CellDictionary {
-    constructor() {
+    };
+    return Cell;
+}());
+var CellDictionary = /** @class */ (function () {
+    function CellDictionary() {
         this.Search = {};
     }
-}
-class TableManager {
-    constructor(cells, htmlCells) {
+    return CellDictionary;
+}());
+var TableManager = /** @class */ (function () {
+    function TableManager(cells, htmlCells) {
         this.cellRows = cells;
         this.htmlCells = htmlCells;
         this.start();
     }
-    getStatesSetColors() {
-        this.cellRows.forEach(row => {
-            row.forEach(cell => {
-                const { currentState } = cell;
-                const key = `${cell.coordinates.positionX}:${cell.coordinates.positionY}`;
-                const htmlCell = this.htmlCells.Search[key];
-                const isLiving = currentState === 1 ? true : false;
-                this.setCellColor(htmlCell, isLiving);
+    TableManager.prototype.getStatesSetColors = function () {
+        var _this = this;
+        this.cellRows.forEach(function (row) {
+            row.forEach(function (cell) {
+                var currentState = cell.currentState;
+                var key = cell.coordinates.positionX + ":" + cell.coordinates.positionY;
+                var htmlCell = _this.htmlCells.Search[key];
+                var isLiving = currentState === 1 ? true : false;
+                _this.setCellColor(htmlCell, isLiving);
             });
         });
-    }
-    setCellColor(htmlCell, isLiving) {
+    };
+    TableManager.prototype.setCellColor = function (htmlCell, isLiving) {
         if (isLiving) {
             htmlCell.classList.remove('cell-dead');
             htmlCell.classList.add('cell-living');
@@ -52,57 +55,79 @@ class TableManager {
             htmlCell.classList.remove('cell-living');
             htmlCell.classList.add('cell-dead');
         }
-    }
-    getNeigboursAndSetNextState(cell) {
-        const { positionX, positionY } = cell.coordinates;
-        const neighbours = [];
+    };
+    TableManager.prototype.getNeigboursAndSetNextState = function (cell) {
+        var _a = cell.coordinates, positionX = _a.positionX, positionY = _a.positionY;
+        var neighbours = [];
         if (positionY - 1 > 0) {
             neighbours.push(this.cellRows[positionX][positionY - 1]);
+            if (positionX - 1 > 0) {
+                neighbours.push(this.cellRows[positionX - 1][positionY - 1]);
+            }
         }
         if (positionX - 1 > 0) {
             neighbours.push(this.cellRows[positionX - 1][positionY]);
+            if (positionY + 1 < 49) {
+                neighbours.push(this.cellRows[positionX - 1][positionY + 1]);
+            }
         }
         if (positionY + 1 < 49) {
             neighbours.push(this.cellRows[positionX][positionY + 1]);
+            if (positionX + 1 < 49) {
+                neighbours.push(this.cellRows[positionX + 1][positionY + 1]);
+            }
         }
         if (positionX + 1 < 49) {
             neighbours.push(this.cellRows[positionX + 1][positionY]);
+            if (positionY - 1 > 0) {
+                neighbours.push(this.cellRows[positionX + 1][positionY - 1]);
+            }
         }
-        const numberOfConnections = neighbours.filter(cell => cell.currentState === CellState.Live).length;
-        if (numberOfConnections < 2 || numberOfConnections > 3) {
-            cell.nextState = CellState.Dead;
+        var numberOfConnections = neighbours.filter(function (cell) { return cell.currentState === CellState.Live; }).length;
+        if (cell.currentState === CellState.Live) {
+            if (numberOfConnections === 2 || numberOfConnections === 3) {
+                cell.nextState = CellState.Live;
+            }
+            else {
+                cell.nextState = CellState.Dead;
+            }
         }
         else {
-            cell.nextState = CellState.Live;
+            if (numberOfConnections === 3) {
+                cell.nextState = CellState.Live;
+            }
         }
-    }
-    setNextStateForTable() {
-        this.cellRows.forEach(row => {
-            row.forEach(cell => {
-                this.getNeigboursAndSetNextState(cell);
+    };
+    TableManager.prototype.setNextStateForTable = function () {
+        var _this = this;
+        this.cellRows.forEach(function (row) {
+            row.forEach(function (cell) {
+                _this.getNeigboursAndSetNextState(cell);
             });
         });
         this.switchToNext();
-    }
-    switchToNext() {
-        this.cellRows.forEach(row => {
-            row.forEach(cell => {
+    };
+    TableManager.prototype.switchToNext = function () {
+        this.cellRows.forEach(function (row) {
+            row.forEach(function (cell) {
                 cell.currentState = cell.nextState;
             });
         });
-    }
-    start() {
-        setInterval(() => {
-            this.getStatesSetColors();
-            this.setNextStateForTable();
+    };
+    TableManager.prototype.start = function () {
+        var _this = this;
+        setInterval(function () {
+            _this.getStatesSetColors();
+            _this.setNextStateForTable();
         }, 500);
-    }
-}
+    };
+    return TableManager;
+}());
 function createCells(size) {
-    const cellRows = [];
-    for (let i = 0; i < size; i++) {
-        const cells = [];
-        for (let j = 0; j < size; j++) {
+    var cellRows = [];
+    for (var i = 0; i < size; i++) {
+        var cells = [];
+        for (var j = 0; j < size; j++) {
             cells.push(new Cell({
                 positionX: i,
                 positionY: j,
@@ -114,17 +139,17 @@ function createCells(size) {
 }
 function setupTable(size) {
     var _a;
-    const tableCells = new CellDictionary();
-    const table = document.createElement('table');
-    for (let i = 0; i < size; i++) {
-        const row = document.createElement('tr');
-        for (let j = 0; j < size; j++) {
-            const data = document.createElement('td');
+    var tableCells = new CellDictionary();
+    var table = document.createElement('table');
+    for (var i = 0; i < size; i++) {
+        var row = document.createElement('tr');
+        for (var j = 0; j < size; j++) {
+            var data = document.createElement('td');
             data.setAttribute('row', i.toString());
             data.setAttribute('col', j.toString());
             setInitialAttribute(data);
             row.appendChild(data);
-            const key = `${i.toString()}:${j.toString()}`;
+            var key = i.toString() + ":" + j.toString();
             tableCells.Search[key] = data;
         }
         table.appendChild(row);
@@ -135,6 +160,6 @@ function setupTable(size) {
 function setInitialAttribute(tableCell) {
     tableCell.classList.add('table-cell');
 }
-const tableCells = setupTable(50);
-const cellRepresentations = createCells(50);
-const manager = new TableManager(cellRepresentations, tableCells);
+var tableCells = setupTable(50);
+var cellRepresentations = createCells(50);
+var manager = new TableManager(cellRepresentations, tableCells);
